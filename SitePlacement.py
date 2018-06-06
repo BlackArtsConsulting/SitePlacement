673574ae997ebc82ad366a5d9e74bb50013a5f54
Anthony Hauck <33582525+hummingburd@users.noreply.github.com>
2018-06-06 15:17:41 -0400

    Added colors and more parameters

--- SitePlacement.py
+++ SitePlacement.py
@@ -33,9 +33,9 @@ buildings = \
     }
 ]
   
-def makeBuilding(site: aecSpace, width: float, rotation: float, area: float):
+def makeBuilding(site: aecSpace, length: float, width: float, height: float, 
+                                 rotation: float, area: float):
     spacer = aecSpacer()
-#    xWidth = buildings[0]['diameter'][random.randint(0, 0)]
     yDepth = width * 1.618
     building = aecSpace()
     building.makeBox(aecPoint(0, 0, 0), xDist = width, yDist = yDepth, zDist = 40)
@@ -55,69 +55,38 @@ def makeSite():
     site.height = 0.1 
     return site     
 
-def sitePlacement(width: float, rotation: float, area: float):
-    
-#    site = aecSpace()
-#    site.boundary = [aecPoint(coord[0], coord[1]) for coord in siteBoundary["coordinates"]]
-#    site.color = aecColor.green
-#    site.level = -0.1
-#    site.height = 0.1
-#    spaces = []
-#    xWidth = buildings[0]['diameter'][random.randint(0, 0)]
-#    yDepth = xWidth * 1.618
-#    building = aecSpace()
-#    building.makeBox(aecPoint(0, 0, 0), xDist = xWidth, yDist = yDepth, zDist = 10)
-#    building.rotate(random.randint(0, 360))
-#    if spacer.placeWithin(building, site):
-#        building.height = buildings[0]['height']
-#        building.level = buildings[0]['level']
-#        building.color = buildings[0]['color']
-#        spaces += [building]
-#        spaces += spacer.stackToArea(building, buildings[0]['area'])
+def sitePlacement(length: float, width: float, height: float, 
+                  rotation: float, area: float):
     site = makeSite()
-    building = makeBuilding(site, width, rotation, area)
+    building = makeBuilding(site, length, width, height, rotation, area)
     area = 0
     floors = 0
     for space in building:
         area += space.floor.area
         floors += 1
-        
-    # Create a model to hold the geometry.
+    # glTF output
     model = glTF()
-
-    # Create materials to use in the model.
-    m_index = model.add_material(0.0, 0.0, 1.0, 0.9, 1.0, "Blue")
-    m_index1 = model.add_material(0.0, 1.0, 0.0, 0.9, 1.0, "Green")
-
+    colorBlue = model.add_material(0.0, 0.631, 0.945, 0.9, 1.0, "Blue")
+    colorGreen = model.add_material(0.486, 0.733, 0.0, 0.9, 0.0, "Green")
+    colorOrange = model.add_material(0.964, 0.325, 0.078, 0.9, 1.0, "Orange")
+    colorYellow = model.add_material(1.0, 0.733, 0.0, 0.9, 1.0, "Yellow")
     spaceMesh = site.mesh_graphic
-    model.add_triangle_mesh(spaceMesh.vertices, spaceMesh.normals, spaceMesh.indices, m_index1)
-    
+    model.add_triangle_mesh(spaceMesh.vertices, spaceMesh.normals, spaceMesh.indices, colorGreen)    
     for space in building:
         spaceMesh = space.mesh_graphic
-        model.add_triangle_mesh(spaceMesh.vertices, spaceMesh.normals, spaceMesh.indices, m_index)
-        
-#        print(spaceMesh.vertices)
-#        for item in spaceMesh.vertices: print(type(item))
-#        print(len(spaceMesh.vertices))
-#        print(spaceMesh.normals)
-#        for item in spaceMesh.normals: print(type(item))        
-#        print(len(spaceMesh.normals))        
-#        print(spaceMesh.indices)
-#        for item in spaceMesh.indices: print(type(item))        
-#        print(len(spaceMesh.indices))        
-#        print(min(spaceMesh.normals))
-#        print(max(spaceMesh.normals))
-#        for item in spaceMesh.normals: print(item)
-#        
-        
-    return {"model": model.save_base64(), 'computed':{'floors':floors, 'area':area}}        
-    
-#    model.save('C:\\Users\\Anthony\\Dropbox\\Business\\BlackArts\\Development\\GitHub\\SitePlacement\\model.gltf')
-#    model.save_glb('C:\\Users\\Anthony\\Dropbox\\Business\\BlackArts\\Development\\GitHub\\SitePlacement\\model.glb')
-    
+        colorIndex = random.randint(0, 2)
+        if colorIndex == 0: color = colorBlue
+        if colorIndex == 1: color = colorOrange
+        if colorIndex == 2: color = colorYellow      
+        model.add_triangle_mesh(spaceMesh.vertices, spaceMesh.normals, spaceMesh.indices, color)   
+    #model.save_glb('C:\\Users\\Anthony\\Dropbox\\Business\\BlackArts\\Development\\GitHub\\SitePlacement\\model.glb')
+    return {"model": model.save_base64(), 'computed':{'floors':floors, 'area':area}}   
 
-    
-# sitePlacement(random.uniform(150, 300), random.uniform(5, 355), random.uniform(100000, 200000))
+#sitePlacement(length = random.uniform(200, 300), 
+#              width = random.uniform(100, 300), 
+#              height = random.uniform(20, 40),
+#              rotation = random.uniform(5, 355), 
+#              area = random.uniform(100000, 200000))
 
 
 
